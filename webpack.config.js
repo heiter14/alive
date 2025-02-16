6cf54ae045d3771779d8c264111f1aae99e61380
heiter14 <73256466+heiter14@users.noreply.github.com>
2025-02-16 16:47:05 +0200

    Create webpack.config.js

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  entry: './js/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'js'),
+  },
+  plugins: [
+    new Dotenv(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader'],
+      },
+    ],
+  },
+};
